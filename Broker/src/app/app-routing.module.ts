7857819c72072d63d7143c07f689f4e5e327cbb9
Lisandro Juncos <lijuncos1@gmail.com>
2023-09-07 19:20:41 -0300

    merge | cotizaciones into develop

--- Broker/src/app/app-routing.module.ts
+++ Broker/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormCompraAccionComponent } from './form-compra-accion/form-compra-accion.component';
+import { CotizacionesComponent } from './components/cotizaciones/cotizaciones.component';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path:'compra', component: FormCompraAccionComponent},
+  {path:'cotizaciones', component: CotizacionesComponent},
+
 ];
 
 @NgModule({
