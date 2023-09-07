4115006c7a1c52ee55fdc453c37c8a3bf0bba4b0
Lisandro Juncos <lijuncos1@gmail.com>
2023-09-07 19:24:09 -0300

    fix | CotizacionesComponent

--- Broker/src/app/components/cotizaciones/cotizaciones.component.ts
+++ Broker/src/app/components/cotizaciones/cotizaciones.component.ts
@@ -13,7 +13,7 @@ export class CotizacionesComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('assets/data/14-06-23.json').subscribe((data) => {
+    this.http.get('assets/json/14-06-23.json').subscribe((data) => {
       this.datos = data;
     });
   }
