2976d755394731308d60dd572bf70d67bd9552b2
Joaquin Colomar <joaquin@colomar.com.ar>
2023-09-07 15:42:11 -0300

    Dashboard y responsive funcionando

--- Broker/src/app/app.module.ts
+++ Broker/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
